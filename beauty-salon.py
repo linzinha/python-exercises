7e87dd98867407439ca04614356b778dd8cf4d82
lmonyelle <45407371+lmonyelle@users.noreply.github.com>
2019-08-02 23:08:48 +0100

    Update beauty-salon.py

--- beauty-salon.py
+++ beauty-salon.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 salonServices = ["shampoo", "cut", "color", "treatment", "style", "nails", "facial hair", "body hair", "facial treatments", "body treatments"]
 
 haircuts = ["pixie", "crew", "asymmetric", "bangs", "bob", "mohawk", "fade", "feathered", "hime", "hi-top fade", "ivy league", "long layers", "lob", "mullet", "fashion mullet", "pageboy", "shag", "undercut"]
@@ -41,29 +44,67 @@ last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
 total_price = 0
 
-for price in prices:
-    total_price += price
-print(total_price)
-
-average_price = total_price / len(hairstyles)
-print("Average Haircut Price: $" + str(average_price))
-
-new_prices = [price - 5 for price in prices]
-print(new_prices)
-
-total_revenue = 0
-
-for i in range(len(hairstyles)):
-    total_revenue += prices[i] * last_week[i]
-
-print("Total Revenue: $" + str(total_revenue))
-average_daily_revenue = total_revenue / 7
-print(average_daily_revenue)
-
-cuts_under_30 = [hairstyles[i] for i in range(len(hairstyles)) if new_prices[i] < 30]
-
-print(cuts_under_30)
+# for price in prices:
+#     total_price += price
+# print(total_price)
+#
+# average_price = total_price / len(hairstyles)
+# print("Average Haircut Price: $" + str(average_price))
+#
+# new_prices = [price - 5 for price in prices]
+# print(new_prices)
+#
+# total_revenue = 0
+#
+# for i in range(len(hairstyles)):
+#     total_revenue += prices[i] * last_week[i]
+#
+# print("Total Revenue: $" + str(total_revenue))
+# average_daily_revenue = total_revenue / 7
+# print(average_daily_revenue)
+#
+# cuts_under_30 = [hairstyles[i] for i in range(len(hairstyles)) if new_prices[i] < 30]
+#
+# print(cuts_under_30)
 
 # sources: http://www.olehenriksenspa.com/services/
 # https://www.bareskindetroit.com/facials
 # https://www.csalons.com/services/
+# https://globalwellnesssummit.com/wp-content/uploads/Industry-Research/Asia-Pacific/2014-colliers-international-uae-spa-market.pdf
+
+firstNames = {
+    1: "Andre",
+    2: "Michael",
+    3: "Jermaine",
+    4: "Susan",
+    5: "Stacey",
+    6: "Dominique",
+    7: "Alejandro",
+    8: "Ali",
+    9: "Fatima",
+    10: "Camila"
+}
+
+lastNames = {
+    1: "Lauder",
+    2: "Beauregard",
+    3: "Beaudelaire",
+    4: "Astor",
+    5: "Urbinati",
+    6: "Canalé",
+    7: "Lecompte",
+    8: "Hatzis",
+    9: "Atelier",
+    10: "Galván"
+}
+owner = random.randint(1, 10)
+print('\nAs you walk into  Luxe; ' + (lastNames[owner]) + ', an adjective pronoun with adjective color eyes greets you at the door.\n')
+print('"Hi, you must be the new receptionist!"\n')
+print('"My name is ' + (firstNames[owner]) + ' ' + (lastNames[owner]) + ', and this is my spa and salon"')
+print('"Since it\'s your first day, I\'ll be showing you the ropes"')
+receptionist = input('"Tell me, what was your name again?" ')
+print('"So lovely to meet you, ' + receptionist + '."')
+print('"Ok, so it\'s just about 10 am, why don\'t you go turn on the sign?"')
+
+
+
