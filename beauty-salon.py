97a969c66a211b8ca231283e7625fb0328ee390e
lmonyelle <45407371+lmonyelle@users.noreply.github.com>
2019-08-02 19:28:55 +0100

    Update beauty-salon.py

--- beauty-salon.py
+++ beauty-salon.py
@@ -1,6 +1,6 @@
 salonServices = ["shampoo", "cut", "color", "treatment", "style", "nails", "facial hair", "body hair", "facial treatments", "body treatments"]
 
-haircuts = ["pixie", "crew", "asymmetric", "bangs", "bob", "mohawk", "fade", "feathered", "hime", "hi-top fade", "ivy league", "long layers", "lob", "mullet", "fashion mullett", "pageboy", "shag", "undercut"]
+haircuts = ["pixie", "crew", "asymmetric", "bangs", "bob", "mohawk", "fade", "feathered", "hime", "hi-top fade", "ivy league", "long layers", "lob", "mullet", "fashion mullet", "pageboy", "shag", "undercut"]
 
 color = ["partial highlight", "all over color", "root touch up", "full highlight", "base color + partial highlight", "base color + full highlight", "partial baylage", "root melt + lowlights", "ombre", "baylage"]
 
@@ -14,11 +14,11 @@ facialHair = ["lip wax", "eyebrow wax", "full face wax", "shave", "facial hair t
 
 bodyHair = ["bikini", "brazilian", "underarm", "shoulders", "stomach", "buttocks", "half legs", "full legs", "half arms", "full arms", "chest", "back", "stomach strip", "chest and stomach", "back and shoulders", "full body"]
 
-facialTreatment = ["deep pore cleansing facial", "brightening facial", "clarity complexion treatment", "red carpet facial", "advanced hydrafacial", "rejuvination treatment", "opulence treatment"]
+facialTreatment = ["deep pore cleansing facial", "brightening facial", "clarity complexion treatment", "red carpet facial", "advanced hydrafacial", "rejuvenation treatment", "opulence treatment"]
 
 facialTreatmentEnhancements = ["healing jade treatment"]
 
-lightTherapies = ["micro photo treatment", "lhe photo rejuvination", "lhe acne", "lhe lifting and tightening"]
+lightTherapies = ["micro photo treatment", "lhe photo rejuvenation", "lhe acne", "lhe lifting and tightening"]
 
 peels = ["vi peel", "vi acne peel", "modified jessner peel", "cosmederm 50% glycolic peel"]
 
@@ -31,6 +31,10 @@ saltSugarSctubs = ["exotic rose + jasmine", "the greek", "the Moroccan", "tradit
 massage = ["return to nature", "manly-sage", "four handed", "ultimate relax", "hot stone", "herbal heat", "craniosacral", "thai", "shiatsu", "swedish", ]
 
 bodyTreatmentEnhancements = ["eco-hin heat therapy", "detoxifying foot soak", "reflexology", "hot stone enhancement"]
+
+skinTypes = ["dry", "oily", "combination"]
+skinConcerns = ["redness", "acne", "oiliness", "dryness", "wrinkles and fine lines", "uneven skin tone", "uneven texture"]
+
 prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
